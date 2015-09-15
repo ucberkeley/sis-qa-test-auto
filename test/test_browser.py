e503c66db25edbcf802e9e0d92412978920f8df6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-09-15 14:51:31 -0700

    Adapt test to pytest

--- test/test_browser.py
+++ test/test_browser.py
@@ -1,34 +1,63 @@
+import http.client
+import socket
+
+import pytest
+
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.remote.command import Command
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
 
-# go to the google home page
-driver.get("http://www.google.com")
+_driver = None
 
-# the page is ajaxy so the title is originally this:
-print driver.title
 
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
+def new_driver():
+    return webdriver.Firefox()
 
-# type in the search
-inputElement.send_keys("cheese!")
 
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
+def setup_module(module):
+    global _driver
+    _driver = new_driver()
 
-try:
+
+def teardown_module(module):
+    global _driver
+    if not has_quit(_driver):
+        _driver.quit()
+
+
+@pytest.fixture(scope='function')
+def driver():
+    global _driver
+    if has_quit(_driver):
+        _driver = new_driver()
+    return _driver
+
+
+def has_quit(driver):
+    try:
+        driver.execute(Command.STATUS)
+        return False
+    except (socket.error, http.client.CannotSendRequest):
+        return True
+
+
+def test_driver(driver):
+    query = "cheese!"
+
+    driver.get("http://www.google.com")
+    assert driver.title == 'Google'
+
+    # find the element that's name attribute is q (the google search box)
+    inputElement = driver.find_element_by_name("q")
+    # type in the search
+    inputElement.send_keys(query)
+    # submit the form (although google automatically searches now without submitting)
+    inputElement.submit()
+
     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-    # You should see "cheese! - Google Search"
-    print driver.title
-
-finally:
-    driver.quit()
+    WebDriverWait(driver, 10).until(EC.title_contains(query))
+    assert driver.title == "{} - Google Search".format(query)
 
 
