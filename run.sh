d9a0d98ec416e662fa8affba3fb0e3a6819e21c4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-09-16 14:10:03 -0700

    Allow for external tests location by setting

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+: ${CC_TEST_DIR:=${PROJECT_DIR}/test}
 
 docker run \
   --rm \
   -t \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v $PROJECT_DIR/test:/test \
+  -v ${CC_TEST_DIR}:/test \
   dibyo/cc-qa-test-auto
