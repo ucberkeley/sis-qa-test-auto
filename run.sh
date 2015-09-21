cfd9e560e7a2cf56b06fe6ee33243aa174c31c8b
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-09-21 15:09:43 -0700

    Clean up whitespace

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${CC_TEST_DIR:=${PROJECT_DIR}/test}
 
 docker run \
-  --rm \
-  --net=host \
-  -t \
-  -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v ${CC_TEST_DIR}:/test \
-  ucberkeley/sis-qa-test-auto
+    --rm \
+    --net=host \
+    -t \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ${CC_TEST_DIR}:/test \
+    ucberkeley/sis-qa-test-auto
