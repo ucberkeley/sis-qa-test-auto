25ed1ee934bc7067db8f28ff517ce9b417ab2831
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-09-27 11:39:56 -0700

    Minor change to run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@
 PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${CC_TEST_DIR:=${PROJECT_DIR}/test}
 
+DOCKER_NAMESPACE=ucberkeley
+
 docker run \
     --rm \
     --net=host \
     -t \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY=${DISPLAY} \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ${CC_TEST_DIR}:/test \
-    ucberkeley/sis-qa-test-auto
+    ${DOCKER_NAMESPACE}/sis-qa-test-auto
