65c47dc90ed7d2d012197bbe4a0da5af9129ded9
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-10-04 02:51:05 -0700

    Remove python-related stuff from .gitignore

--- sync.sh
+++ sync.sh
@@ -13,8 +13,7 @@ DOCKER_IMAGE_DIR=${PROJECT_DIR}/docker/sis-qa-test-auto
 if [ -f ${SIS_TEST_DIR}/Gemfile ] && ! cmp --silent ${SIS_TEST_DIR}/Gemfile ${DOCKER_IMAGE_DIR}/Gemfile; then
     echo "Gemfiles different. Syncing and rebuilding Docker image..."
     cp ${SIS_TEST_DIR}/Gemfile ${DOCKER_IMAGE_DIR}
-    if [ -f ${SIS_TEST_DIR}/Gemfile.lock ]
-    then
+    if [ -f ${SIS_TEST_DIR}/Gemfile.lock ]; then
         cp ${SIS_TEST_DIR}/Gemfile.lock ${DOCKER_IMAGE_DIR}
     fi
     sudo docker build -t ${DOCKER_NAMESPACE}/sis-qa-test-auto ${DOCKER_IMAGE_DIR}
