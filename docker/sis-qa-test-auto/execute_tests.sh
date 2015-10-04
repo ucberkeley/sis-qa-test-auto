371d6aa655347548f108acdbbff8eecdf1bae185
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-10-03 18:39:18 -0700

    Add parsing of .config.json file

--- docker/sis-qa-test-auto/execute_tests.sh
+++ docker/sis-qa-test-auto/execute_tests.sh
@@ -2,4 +2,5 @@
 source /etc/profile.d/rvm.sh
 cd /test
 bundle install
+export SIS_TEST_DIR=/test
 bundle exec cucumber
--- test/features/support/hooks.rb
+++ test/features/support/hooks.rb
@@ -1,4 +1,15 @@
+require 'json'
+require 'pathname'
+
 require 'selenium-webdriver'
+
+
+SIS_TEST_DIR_ENV = 'SIS_TEST_DIR'
+if ENV.has_key? SIS_TEST_DIR_ENV
+  $config = JSON.parse(Pathname.new(ENV[SIS_TEST_DIR_ENV]).join('.config.json').read)
+else
+  $config = JSON.parse(Pathname.new(__FILE__).dirname.dirname.dirname.join('.config.json').read)
+end
 
 Before do
   $driver = Selenium::WebDriver.for :firefox
