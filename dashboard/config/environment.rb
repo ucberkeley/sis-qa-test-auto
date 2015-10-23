0151fe17a4cef6147a1dc3aeba1ac80ede7215f4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-10-23 12:59:53 -0700

    Upgrade to ruby-2.2.3

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -3,10 +3,10 @@ SIS_TEST_SERVER_PORT_ENV = 'SIS_TEST_SERVER_PORT'
 
 # Check and set (if required) environment variables
 unless ENV.key? SIS_TEST_DIR_ENV
-  ENV[SIS_TEST_DIR_ENV] = Pathname.new(__FILE__).dirname.dirname.dirname.join('logs')
+  ENV[SIS_TEST_DIR_ENV] = Pathname.new(__FILE__).dirname.dirname.dirname.join('logs').to_s
 end
 unless ENV.key? SIS_TEST_SERVER_PORT_ENV
-  ENV[SIS_TEST_SERVER_PORT_ENV] = 8421
+  ENV[SIS_TEST_SERVER_PORT_ENV] = 8421.to_s
 end
 
 
