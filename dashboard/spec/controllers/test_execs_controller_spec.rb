289e9e032d5b6b9a352d4b0ff5ecf41614bb1e8b
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-11-05 20:48:09 -0800

    Add basic tests for TestExecs controller

--- dashboard/spec/controllers/test_execs_controller_spec.rb
+++ dashboard/spec/controllers/test_execs_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe TestExecsController, :type => :controller do
 
+  describe 'GET index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET show' do
+    it 'returns http success' do
+      get :show, :id => 123455678
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET create' do
+    it 'returns http success' do
+      get :create
+      expect(response).to redirect_to :test_execs
+    end
+  end
+
 end
