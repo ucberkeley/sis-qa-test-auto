69c47e868c326ea8397d818a65153fb81def25e1
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-11-16 17:59:32 -0800

    Add SIS_TEST_WEBDRIVER as a config variable for choosing web driver

--- test/features/support/config.rb
+++ test/features/support/config.rb
@@ -2,7 +2,6 @@ require 'json'
 require 'pathname'
 
 require 'capybara/cucumber'
-require 'capybara/poltergeist'
 
 
 SIS_TEST_DIR_ENV = 'SIS_TEST_DIR'
@@ -12,6 +11,17 @@ else
   $config = JSON.parse(Pathname.new(__FILE__).dirname.dirname.dirname.join('.config.json').read)
 end
 
-Capybara.default_driver = :poltergeist
+
+SIS_TEST_WEBDRIVER_ENV = 'SIS_TEST_WEBDRIVER'
+if ENV.has_key? SIS_TEST_WEBDRIVER_ENV and ENV[SIS_TEST_WEBDRIVER_ENV].downcase == 'selenium'
+  Bundler.require(:default, :webdriver_selenium)
+  require 'selenium/webdriver'
+  Capybara.default_driver = :selenium
+else
+  Bundler.require(:default, :webdriver_poltergeist)
+  require 'capybara/poltergeist'
+  Capybara.default_driver = :poltergeist
+end
+
 Capybara.default_wait_time = 5
 
