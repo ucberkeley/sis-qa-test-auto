fa2dcc7067c02f7d2b8b0a48932aef051bb9137e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-11-18 11:47:59 -0800

    Move server Dockerfile to server folder

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,26 +8,13 @@ fi
 : ${DOCKER_NAMESPACE:=ucberkeley}
 project_dir=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd )
 : ${SIS_TEST_DIR:=${project_dir}/test}
-DOCKER_IMAGE_DIR=${project_dir}/docker/sis-qa-test-auto
+SIS_SERVER_DIR=${project_dir}/server
 
-stale=false
-
-diff -br ${project_dir}/server ${DOCKER_IMAGE_DIR}/server &> /dev/null;
-if [ $? -ne 0 ]; then
-  stale=true
-  rm -rf ${DOCKER_IMAGE_DIR}/server
-  cp -r ${project_dir}/server ${DOCKER_IMAGE_DIR}
-fi
-
-if [ -f ${SIS_TEST_DIR}/Gemfile ] && ! cmp --silent ${SIS_TEST_DIR}/Gemfile ${DOCKER_IMAGE_DIR}/Gemfile; then
-  stale=true
-  cp ${SIS_TEST_DIR}/Gemfile ${DOCKER_IMAGE_DIR}
+if [ -f ${SIS_TEST_DIR}/Gemfile ] && ! cmp --silent ${SIS_TEST_DIR}/Gemfile ${SIS_SERVER_DIR}/Gemfile; then
+  cp ${SIS_TEST_DIR}/Gemfile ${SIS_SERVER_DIR}
   if [ -f ${SIS_TEST_DIR}/Gemfile.lock ]; then
-      cp ${SIS_TEST_DIR}/Gemfile.lock ${DOCKER_IMAGE_DIR}
+    cp ${SIS_TEST_DIR}/Gemfile.lock ${SIS_SERVER_DIR}
   fi
-fi
-
-if ${stale}; then
   echo "Docker image stale. Synced and rebuilding..."
-  sudo docker build -t ${DOCKER_NAMESPACE}/sis-qa-test-auto ${DOCKER_IMAGE_DIR}
+  sudo docker build -t ${DOCKER_NAMESPACE}/sis-qa-test-auto ${SIS_SERVER_DIR}
 fi
