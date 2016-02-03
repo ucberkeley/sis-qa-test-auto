7d96d5a9ab62d0ab156f2b81c595885ff779b618
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-02-03 00:43:44 -0800

    Optimize gulpfile

--- dashboard/gulpfile.js
+++ dashboard/gulpfile.js
@@ -8,6 +8,17 @@
   var jshint = require('gulp-jshint');
   var jscs = require('gulp-jscs');
 
+  var paths = {
+    src: {
+      js: './**/*.js',
+      scss: 'app/assets/stylesheets/*.js'
+    },
+    dest: {
+      js: 'public/assets/js',
+      css: 'public/assets/css'
+    }
+  };
+
   gulp.task('default', ['all']);
 
   gulp.task('all', function(callback) {
@@ -21,36 +32,34 @@
 
   gulp.task('test', ['jscs', 'jshint']);
 
-  gulp.task('build', ['browserify']);
+  gulp.task('build', ['browserify', 'scss']);
 
   gulp.task('jshint', function() {
-    return gulp.src('./**/*.js')
+    return gulp.src(paths.src.js)
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'))
       .pipe(jshint.reporter('fail'));
   });
 
   gulp.task('jscs', function() {
-    return gulp.src('./**/*.js')
+    return gulp.src(paths.src.js)
       .pipe(jscs())
       .pipe(jscs.reporter('jscs-stylish'))
       .pipe(jscs.reporter('fail'));
   });
 
   gulp.task('browserify', function() {
-    return browserify([
-      'app/assets/javascripts/app.js'
-    ], {
+    return browserify(['app/assets/javascripts/app.js'], {
       debug: true
     })
       .bundle()
       .pipe(source('app.js'))
-      .pipe(gulp.dest('public/assets/javascript'));
+      .pipe(gulp.dest(paths.dest.js));
   });
 
   gulp.task('scss', function() {
     return gulp.src('app/assets/stylesheets/app.scss')
       .pipe(scss())
-      .pipe(gulp.dest('public/assets/css'));
-  })
+      .pipe(gulp.dest(paths.dest.css));
+  });
 })();
