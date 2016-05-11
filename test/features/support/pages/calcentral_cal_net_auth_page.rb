7d02d26ada2a17d1f1bae62d0f8e81b757430e1d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-05-11 15:25:59 -0700

    Explicitly log out between tests

--- test/features/support/pages/calcentral_cal_net_auth_page.rb
+++ test/features/support/pages/calcentral_cal_net_auth_page.rb
@@ -2,19 +2,25 @@ class CalcentralCalNetAuthPage < SitePrism::Page
 
   set_url $config['calcentralCalNetAuthPageUrl']
 
-  element :username_field, 'input[id="username"]'
-  element :password_field, 'input[id="password"]'
-  element :submit_button, 'input[value="Sign In"]'
+  element :username_field, :xpath, '//input[@id="username"]'
+  element :password_field, :xpath, '//input[@id="password"]'
+  element :submit_button, :xpath, '//input[@value="Sign In"]'
+  element :log_out_button, :xpath, '//button[contains(text(), "Log out")]'
+  element :settings_button, :xpath, '//button[@title="Settings"]'
 
   def initialize
     self.load
   end
 
   def login(username, password)
-    if self.has_username_field?
-      self.username_field.set username
-      self.password_field.set password
-      self.submit_button.click
+    unless self.has_username_field?
+      self.settings_button.click
+      self.log_out_button.click
+      sleep 1
+      self.load
     end
+    self.username_field.set username
+    self.password_field.set password
+    self.submit_button.click
   end
 end
